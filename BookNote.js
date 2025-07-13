7f9f283c4d00f086e900c4f254d2731597f18034
Andrew Noble <aknoble.andrew@gmail.com>
2025-07-13 08:19:09 -0700

    started skeleton code for book notes page

---
+++ BookNote.js
@@ -0,0 +1,18 @@
+export function createBookNoteComponent(title, author, rating, notes) {
+  const bookNoteElement = document.createElement("div");
+  const t = document.createElement("h2");
+  t.textContent = title;
+  const a = document.createElement("p");
+  a.textContent = author;
+  const r = document.createElement("p");
+  r.textContent = rating;
+  const n = document.createElement("p");
+  n.textContent = notes;
+
+  bookNoteElement.appendChild(t);
+  bookNoteElement.appendChild(a);
+  bookNoteElement.appendChild(r);
+  bookNoteElement.appendChild(n);
+
+  return bookNoteElement;
+}
